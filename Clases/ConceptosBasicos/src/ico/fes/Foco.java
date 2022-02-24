426fd5dd016c1ad11c82b794e98ae3fd78dbc18a
viktorgervacio21 <viktorgervacio@gmail.com>
2022-02-24 12:52:18 -0600

    iniciamos el archivo foco

--- Clases/ConceptosBasicos/src/ico/fes/Foco.java
+++ Clases/ConceptosBasicos/src/ico/fes/Foco.java
@@ -9,5 +9,50 @@ package ico.fes;
  * @author walmart
  */
 public class Foco {
+    private String color;
+    private String tipo;
+    private int tamano;
+
+    public Foco() {
+    }
+
+    public Foco(String color, String tipo, int tamano) {
+        this.color = color;
+        this.tipo = tipo;
+        this.tamano = tamano;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Foco{" + "color=" + color + ", tipo=" + tipo + ", tamano=" + tamano + '}';
+    }
     
-}
+   
+    }
+    
+    
+
