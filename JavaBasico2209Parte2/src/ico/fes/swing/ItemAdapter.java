d797b65b9360bf138984e9b2e82b098fc69a59fa
viktorgervacio21 <91903398+viktorgervacio21@users.noreply.github.com>
2022-05-26 14:08:05 -0500

    Add files via upload

---
+++ JavaBasico2209Parte2/src/ico/fes/swing/ItemAdapter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.swing;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+/**
+ *
+ * @author walmart
+ */
+public class ItemAdapter implements ItemListener {
+     @Override
+    public void itemStateChanged(ItemEvent ie) {
+    }
+    
+    
+}
